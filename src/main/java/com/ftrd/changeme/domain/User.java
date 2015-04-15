2cfa07775d21bb381161de1ba50efac7969860ea
Teresa Jimenez Arreola <teresilla@gmail.com>
2015-04-15 14:13:52 +0100

    adding User and UserActivity beans

---
+++ src/main/java/com/ftrd/changeme/domain/User.java
@@ -0,0 +1,40 @@
+package com.ftrd.changeme.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+/**
+ * The persistent class for the event_log database table.
+ * 
+ */
+@Entity
+@Table(name="user")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+ 
+	@Column(name="User_Name")
+	private String userName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
---
+++ src/main/java/com/ftrd/changeme/domain/UserActivity.java
@@ -0,0 +1,82 @@
+package com.ftrd.changeme.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+/**
+ * The persistent class for the event_log database table.
+ * 
+ */
+@Entity
+@Table(name="user")
+public class UserActivity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+ 
+	@Column(name="User_Name")
+	private String userName;
+	
+	@ManyToOne
+	@JoinColumn(name="User_Id")
+	private User user;
+	
+//	@ManyToMany
+//	@JoinColumn(name="Activity_Id")
+	//private Activity Activity_Id ;
+	
+	@Column(name="Points")
+	private int points;
+	
+	@Column(name="Activity_Timestamp")
+	private Timestamp timeStamp;
+
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public Timestamp getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(Timestamp timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+
+
+}
