8097c799e021fc6a42606f1f71851685d536700f
Teresa Jimenez Arreola <teresilla@gmail.com>
2015-04-15 16:14:19 +0100

    adding rest api

--- src/main/java/com/ftrd/changeme/front/HomeController.java
+++ src/main/java/com/ftrd/changeme/front/HomeController.java
@@ -1,13 +1,17 @@
 package com.ftrd.changeme.front;
 
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.ftrd.changeme.services.UserActivityManager;
 import com.ftrd.changeme.services.UserManager;
+import com.google.gson.JsonObject;
 
 /**
  * @author Your Name
@@ -15,15 +19,33 @@ import com.ftrd.changeme.services.UserManager;
  */
 @Controller
 @RequestMapping("/")
-@SessionAttributes("user")
 public class HomeController {
-	private Logger log = Logger.getLogger(getClass());
 	@Autowired
-	private UserManager userManager;
-
+	private UserActivityManager userActivityManager;
+	
+	private Logger log = Logger.getLogger(getClass());
+	
+	
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index() {
 		return "index";
 	}
+	
+	@RequestMapping(value = "/activity", method = RequestMethod.POST, consumes="application/json")
+	public String saveUserActivity(@RequestBody String json) {
+		JSONObject jsonObject = new JSONObject(json);
+		int userId = (Integer) jsonObject.get("user_id");
+		System.out.println("user id"+ userId);
+		int activityId = (Integer) jsonObject.get("activity_id");
+		System.out.println("activity  id"+activityId);
+		double distance = (Double) jsonObject.get("distance");
+		System.out.println("distance "+distance);
+
+		userActivityManager.saveUserActivity(userId, activityId, distance);
+		
+		return "index";
+	}
+	
+	
 
 }
